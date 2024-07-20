6d142ff9d3f1a4b76ec99c7b55438b7f00c47b2b
Freesca <checco1009@gmail.com>
2024-07-20 18:06:30 +0200

    test2

--- main.c
+++ main.c
@@ -6,10 +6,19 @@
 /*   By: fdonati <fdonati@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 17:51:10 by fdonati           #+#    #+#             */
-/*   Updated: 2024/07/20 17:51:33 by fdonati          ###   ########.fr       */
+/*   Updated: 2024/07/20 18:05:28 by fdonati          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	main(int argc, char **argv, char **env)
+{
+	t_env	*env;
+	t_cmd	*cmd;
+
+	env = NULL;
+	cmd = NULL;
+}
 
 //TODO: boh
