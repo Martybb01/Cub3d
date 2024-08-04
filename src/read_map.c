bdbb82494a5dd54448f4fa52d546067868419b58
Martina Boccuzzi <martybb01@gmail.com>
2024-08-04 12:44:00 +0200

    brutto e da sistemare e norminettare ma funzia

--- src/read_map.c
+++ src/read_map.c
@@ -6,11 +6,12 @@
 /*   By: marboccu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 11:49:39 by fdonati           #+#    #+#             */
-/*   Updated: 2024/07/27 10:28:48 by marboccu         ###   ########.fr       */
+/*   Updated: 2024/08/04 11:40:47 by marboccu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+#include <ctype.h>
 
 static int	ft_blank_line(char *line)
 {
@@ -26,6 +27,88 @@ static int	ft_blank_line(char *line)
 	return (0);
 }
 
+int	ft_parse_color(char *line, t_rgb *color)
+{
+	char	*tmp;
+	int		len;
+	char **rgb;
+
+	tmp = ft_strtrim(line + 2, " \t");
+	if (!tmp)
+		return (1);
+	len = ft_strlen(tmp);
+	if (len > 0 && tmp[len - 1] == '\n')
+		tmp[len - 1] = '\0';
+	printf("%s\n", tmp);
+	rgb = ft_split(tmp, ',');
+	if (!rgb || !rgb[0] || !rgb[1] || !rgb[2] || rgb[3])
+		ft_err(BAD_COLOR, 1);
+
+	if (!ft_isdigit(rgb[0][0]) || !ft_isdigit(rgb[1][0]) || !ft_isdigit(rgb[2][0]))
+		ft_err(BAD_COLOR, 1);
+
+	if (ft_atoi(rgb[0]) < 0 || ft_atoi(rgb[0]) > 255 || ft_atoi(rgb[1]) < 0 || ft_atoi(rgb[1]) > 255 || ft_atoi(rgb[2]) < 0 || ft_atoi(rgb[2]) > 255)
+		ft_err(BAD_COLOR, 1);
+	
+	color->r = ft_atoi(rgb[0]);
+	color->g = ft_atoi(rgb[1]);
+	color->b = ft_atoi(rgb[2]);
+	printf("r: %d, g: %d, b: %d\n", color->r, color->g, color->b);
+
+	ft_free_matrix(rgb);
+	free(tmp);
+	return (0);
+}
+
+int	ft_parse_texture(char *line, t_texture *texture, t_var *mlx)
+{
+	char	*tmp;
+	int len;
+
+	tmp = ft_strtrim(line + 2, " \t");
+	if (!tmp)
+		return (1);
+	len = ft_strlen(tmp);
+	if (len > 0 && tmp[len - 1] == '\n')
+		tmp[len - 1] = '\0';
+	printf("%s\n", tmp);
+	if (!ft_match_ext(tmp, ".xpm"))
+	{
+		printf("Extension error: %s\n", tmp);
+		ft_err(EXT_ERROR, 1);
+	}
+	if (texture->img)
+	{
+		mlx_destroy_image(mlx, texture->img);
+		texture->img = NULL;
+	}
+	texture->img = mlx_xpm_file_to_image(mlx, tmp, &texture->width, &texture->height);
+	free(tmp);
+	if (!texture->img)
+		return (1);
+	texture->addr = mlx_get_data_addr(texture->img, &texture->bpp, &texture->line_length, &texture->endian);
+	if (!texture->addr)
+		return (1);
+	return (0);
+}
+
+int ft_check_options(char *line, t_var *var)
+{
+	if (line[0] == 'F')
+		return (ft_parse_color(line, &var->pars.floor));
+	if (line[0] == 'C')
+		return (ft_parse_color(line, &var->pars.ceiling));
+	if (line[0] == 'N' && line[1] == 'O')
+		return (ft_parse_texture(line, &var->pars.no, var));
+	if (line[0] == 'S' && line[1] == 'O')
+		return (ft_parse_texture(line, &var->pars.so, var));
+	if (line[0] == 'W' && line[1] == 'E')
+		return (ft_parse_texture(line, &var->pars.we, var));
+	if (line[0] == 'E' && line[1] == 'A')
+		return (ft_parse_texture(line, &var->pars.ea, var));
+	return (0);
+}
+
 int	ft_read_map(char *path, t_var *var)
 {
 	int		fd;
@@ -37,13 +120,15 @@ int	ft_read_map(char *path, t_var *var)
 	i = 0;
 	fd = open(path, O_RDONLY);
 	if (fd == -1)
-		ft_error(3, var);
+		return (ft_err(FILE_ERROR, 1));
 	buffer = ft_strdup("");
 	while (1)
 	{
 		line = ft_get_next_line(fd);
 		if (!line)
 			break ;
+		if (ft_check_options(line, var) == 1)
+			return (ft_err(BAD_OPTION, 1));
 		if (ft_blank_line(line))
 			i++;
 		if (i > 6)
--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: marboccu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 16:02:06 by marboccu          #+#    #+#             */
-/*   Updated: 2024/07/28 19:49:20 by marboccu         ###   ########.fr       */
+/*   Updated: 2024/08/04 11:48:03 by marboccu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,9 +36,9 @@ int	ft_check_file(int ac, char *file)
 {
 	(void)file;
 	if (ac != 2)
-		ft_error(1, NULL);
+		ft_err(BAD_ARG, 1);
 	else if (!ft_match_ext(file, ".cub"))
-		ft_error(3, NULL);
+		ft_err(EXT_ERROR, 1);
 	return (0);
 } 
 
@@ -51,4 +51,9 @@ void ft_trim_leadblank(char *str)
 		start++;
 	if (start != str)
 		ft_memmove(str, start, ft_strlen(start) + 1);
+}
+
+int create_trgb(int t, int r, int g, int b)
+{
+	return (t << 24 | r << 16 | g << 8 | b);
 }
